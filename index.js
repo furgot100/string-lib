96d349595ef104300184ec4743dc3f3d76c6f4b5
Francis Tsang <frtsang40@gmail.com>
2022-02-27 11:32:11 -0800

    started elsint

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 // 1
 function capitalize(str) {
-    return str[0].toUpperCase() + str.slice(1);
+  return str[0].toUpperCase() + str.slice(1);
 }
 // console.log(capitalize('hello world'))
 // 2
 function allCaps(str) {
-    return str.toUpperCase();
+  return str.toUpperCase();
 }
 // console.log(allCaps('foo bar'))
 // 3
 function capitalizeWords(str) {
-    return str.split(' ').map(capitalize).join(' ');
+  return str.split(' ').map(capitalize).join(' ');
 }
 // console.log(capitalizeWords('do all the things'))
 // 4
 function removeExtraSpaces(str) {
-    return str.trim(str).split(/\s+/).join(' ');
+  return str.trim(str).split(/\s+/).join(' ');
 }
 // console.log(removeExtraSpaces("   Hello    world!   "))
 // 5
 function kebobCase(string, separator) {
-    if (separator === void 0) { separator = '-'; }
-    var lower = string.toLowerCase();
-    var char = lower.split("");
-    var filter = char.filter(function (charater) {
-        var code = charater.charCodeAt(0);
+  if (separator === undefined) { separator = '-'; }
+  const lower = string.toLowerCase();
+  const char = lower.split('');
+  const filter = char.filter(function (charater) {
+      const code = charater.charCodeAt(0);
         if (code > 96 && code < 123) {
             return true;
         }
@@ -36,32 +36,32 @@ function kebobCase(string, separator) {
         }
         return false;
     });
-    var noSpace = removeExtraSpaces(filter.join(''));
-    return noSpace.split(' ').join(separator);
+  const noSpace = removeExtraSpaces(filter.join(''));
+  return noSpace.split(' ').join(separator);
 }
 // console.log(kebobCase("Hello world"))
 // Challenge 6
 function snakeCase(string) {
-    return kebobCase(string, '_');
+  return kebobCase(string, '_');
 }
 // console.log(snakeCase("what the heck "))
 // 7
 function camelCase(string) {
-    var array = string.trim().split(" ");
-    var first = array[0].toLowerCase();
-    var remainder = array.slice(1).map(capitalize).join("");
-    return [first, remainder].join("");
+  const array = string.trim().split(' ');
+  const first = array[0].toLowerCase();
+  const remainder = array.slice(1).map(capitalize).join('');
+  return [first, remainder].join('');
 }
 // console.log(camelCase("Camel Case"))
 // 8
 function shift(string, charSpaces) {
-    return string.slice(charSpaces) + string.slice(0, charSpaces);
+  return string.slice(charSpaces) + string.slice(0, charSpaces);
 }
 // console.log(shift("foo bare", 3))
 // 10
 function empty(string) {
-    var trimmed = string.trim();
-    return trimmed.split(/\s+/).join("").length === 0;
+  const trimmed = string.trim();
+  return trimmed.split(/\s+/).join('').length === 0;
 }
 // console.log(empty("Abc def"))
 // console.log(empty(" "))
